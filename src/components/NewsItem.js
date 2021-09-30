e6d9b5839611fc177ff6a641d78ccdaaa051c343
chirayupant <chirayupant@yahoo.com>
2021-09-30 23:54:40 +0530

    Add news source

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,22 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-    render() {
-        let {title,description,imageUrl,newsUrl,author,date} = this.props;
-        return (
-            <div className="my-3">
-                <div className="card">
-                    <img src={!imageUrl?"https://images.indianexpress.com/2021/09/horoscope-5-1.jpeg":imageUrl} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <p className="card-text"><small className="text-muted">By {author?author:"Unknown"} on {new Date(date).toGMTString()}</small></p>
-                        <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read Me</a>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <img
+            src={
+              !imageUrl
+                ? "https://images.indianexpress.com/2021/09/horoscope-5-1.jpeg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%',zIndex: '1'}}>
+              {source}
+            </span>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author ? author : "Unknown"} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-primary"
+            >
+              Read Me
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NewsItem
+export default NewsItem;
